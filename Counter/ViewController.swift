e81dec6a0522bde713008269044b4e4db98e00cc
Aleksandra Velikaia <sashuunka@yandex.ru>
2023-12-22 17:09:59 +0300

    Создан счетчик и кнопка

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pressButton: UIButton!
+    @IBOutlet weak var counterLabelOfPress: UILabel!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabelOfPress.text = "Значение счетчика: 0"
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+      }
 
-
+    @IBAction func buttonDidPress(_ sender: Any) {
+        count += 1
+        counterLabelOfPress.text = "Значение счетчика: " + String(count)
+    }
+    
 }
 
